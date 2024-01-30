9168604149005743b896655af9495fcb56810a21
LilANARY <138551277+LilANARY@users.noreply.github.com>
2024-01-30 11:02:25 +0800

    Add files via upload

---
+++ 时间安排以及分工决定.mm
@@ -0,0 +1,96 @@
+
+<map>
+  <node ID="root" TEXT="时间安排以及分工决定">
+    <node TEXT="名单决定：" ID="XsG2DKjjXZ" _mubu_text="%3Cspan%20class=%22bold%22%3E%E5%90%8D%E5%8D%95%E5%86%B3%E5%AE%9A%EF%BC%9A%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="ui设计：卢江莲" ID="2eP72lzkRh" _mubu_text="%3Cspan%3Eui%E8%AE%BE%E8%AE%A1%EF%BC%9A%E5%8D%A2%E6%B1%9F%E8%8E%B2%3C/span%3E" STYLE="fork"/>
+      <node TEXT="前端：潘辉" ID="utUxhjbgeN" _mubu_text="%3Cspan%3E%E5%89%8D%E7%AB%AF%EF%BC%9A%E6%BD%98%E8%BE%89%3C/span%3E" STYLE="fork"/>
+      <node TEXT="后端三人：唐梓庭，陈梓涛，谢宗翰" ID="dPtBnVfVZ5" _mubu_text="%3Cspan%3E%E5%90%8E%E7%AB%AF%E4%B8%89%E4%BA%BA%EF%BC%9A%E5%94%90%E6%A2%93%E5%BA%AD%EF%BC%8C%E9%99%88%E6%A2%93%E6%B6%9B%EF%BC%8C%E8%B0%A2%E5%AE%97%E7%BF%B0%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="主界面，登陆界面，注册界面，主界面，出租/借出，个人信息" ID="PabEhob42E" _mubu_text="%3Cspan%20class=%22%20text-color-green%22%3E%E4%B8%BB%E7%95%8C%E9%9D%A2%EF%BC%8C%E7%99%BB%E9%99%86%E7%95%8C%E9%9D%A2%EF%BC%8C%E6%B3%A8%E5%86%8C%E7%95%8C%E9%9D%A2%EF%BC%8C%E4%B8%BB%E7%95%8C%E9%9D%A2%EF%BC%8C%E5%87%BA%E7%A7%9F/%E5%80%9F%E5%87%BA%EF%BC%8C%E4%B8%AA%E4%BA%BA%E4%BF%A1%E6%81%AF%3C/span%3E" STYLE="bubble" POSITION="right"/>
+    <node TEXT="项目难点:" ID="uF41FFOD9Y" _mubu_text="%3Cspan%20class=%22bold%20highlight-yellow%22%3E%E9%A1%B9%E7%9B%AE%E9%9A%BE%E7%82%B9:%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="硬件远程锁车技术，显示电动车电池电量 -- 最麻烦的点，目前未知" ID="fDq5Cjy6Qo" _mubu_text="%3Cspan%3E%E7%A1%AC%E4%BB%B6%E8%BF%9C%E7%A8%8B%E9%94%81%E8%BD%A6%E6%8A%80%E6%9C%AF%EF%BC%8C%E6%98%BE%E7%A4%BA%E7%94%B5%E5%8A%A8%E8%BD%A6%E7%94%B5%E6%B1%A0%E7%94%B5%E9%87%8F%20--%20%E6%9C%80%E9%BA%BB%E7%83%A6%E7%9A%84%E7%82%B9%EF%BC%8C%E7%9B%AE%E5%89%8D%E6%9C%AA%E7%9F%A5%3C/span%3E" STYLE="fork"/>
+      <node TEXT="充电桩是否要纳入，如何纳入(导航？(或许只用标点位不需要导航)/去该地点借电动车？)" ID="NamTqSk9W2" _mubu_text="%3Cspan%3E%E5%85%85%E7%94%B5%E6%A1%A9%E6%98%AF%E5%90%A6%E8%A6%81%E7%BA%B3%E5%85%A5%EF%BC%8C%E5%A6%82%E4%BD%95%E7%BA%B3%E5%85%A5(%E5%AF%BC%E8%88%AA%EF%BC%9F(%E6%88%96%E8%AE%B8%E5%8F%AA%E7%94%A8%E6%A0%87%E7%82%B9%E4%BD%8D%E4%B8%8D%E9%9C%80%E8%A6%81%E5%AF%BC%E8%88%AA)/%E5%8E%BB%E8%AF%A5%E5%9C%B0%E7%82%B9%E5%80%9F%E7%94%B5%E5%8A%A8%E8%BD%A6%EF%BC%9F)%3C/span%3E" STYLE="fork"/>
+      <node TEXT="gps定位技术 -- 考虑使用第三方gps定位器去实验是否可以导航" ID="wrnlCKFzV4" _mubu_text="%3Cspan%3Egps%E5%AE%9A%E4%BD%8D%E6%8A%80%E6%9C%AF%20--%20%E8%80%83%E8%99%91%E4%BD%BF%E7%94%A8%E7%AC%AC%E4%B8%89%E6%96%B9gps%E5%AE%9A%E4%BD%8D%E5%99%A8%E5%8E%BB%E5%AE%9E%E9%AA%8C%E6%98%AF%E5%90%A6%E5%8F%AF%E4%BB%A5%E5%AF%BC%E8%88%AA%3C/span%3E" STYLE="fork"/>
+      <node TEXT="微信支付接口api -- 网上可以搜" ID="Ltcxv3l6tj" _mubu_text="%3Cspan%3E%E5%BE%AE%E4%BF%A1%E6%94%AF%E4%BB%98%E6%8E%A5%E5%8F%A3api%20--%20%E7%BD%91%E4%B8%8A%E5%8F%AF%E4%BB%A5%E6%90%9C%3C/span%3E" STYLE="fork"/>
+      <node TEXT="聊天信息平台 -- 网上可以搜" ID="zohR9pJPPd" _mubu_text="%3Cspan%3E%E8%81%8A%E5%A4%A9%E4%BF%A1%E6%81%AF%E5%B9%B3%E5%8F%B0%20--%20%E7%BD%91%E4%B8%8A%E5%8F%AF%E4%BB%A5%E6%90%9C%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="后端选择：" ID="AVWZUnb5lI" _mubu_text="%3Cspan%20class=%22bold%20text-color-red%22%3E%E5%90%8E%E7%AB%AF%E9%80%89%E6%8B%A9%EF%BC%9A%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="唐梓庭:接口文档+聊天信息+微信支付接口api" ID="FtyMnisKY1" _mubu_text="%3Cspan%3E%E5%94%90%E6%A2%93%E5%BA%AD:%E6%8E%A5%E5%8F%A3%E6%96%87%E6%A1%A3+%E8%81%8A%E5%A4%A9%E4%BF%A1%E6%81%AF+%E5%BE%AE%E4%BF%A1%E6%94%AF%E4%BB%98%E6%8E%A5%E5%8F%A3api%3C/span%3E" STYLE="fork"/>
+      <node TEXT="陈梓涛:登陆界面表单和注册界面表单+gps定位" ID="9EPGllY8Ff" _mubu_text="%3Cspan%3E%E9%99%88%E6%A2%93%E6%B6%9B:%E7%99%BB%E9%99%86%E7%95%8C%E9%9D%A2%E8%A1%A8%E5%8D%95%E5%92%8C%E6%B3%A8%E5%86%8C%E7%95%8C%E9%9D%A2%E8%A1%A8%E5%8D%95+gps%E5%AE%9A%E4%BD%8D%3C/span%3E" STYLE="fork"/>
+      <node TEXT="谢宗翰:个人信息表单" ID="gpCDSd69qt" _mubu_text="%3Cspan%3E%E8%B0%A2%E5%AE%97%E7%BF%B0:%E4%B8%AA%E4%BA%BA%E4%BF%A1%E6%81%AF%E8%A1%A8%E5%8D%95%3C/span%3E" STYLE="fork"/>
+      <node TEXT="账户名下电动车管理信息(查询、分页、响应返回数据和请求)" ID="ffkDPldBtk" _mubu_text="%3Cspan%3E%E8%B4%A6%E6%88%B7%E5%90%8D%E4%B8%8B%E7%94%B5%E5%8A%A8%E8%BD%A6%E7%AE%A1%E7%90%86%E4%BF%A1%E6%81%AF(%E6%9F%A5%E8%AF%A2%E3%80%81%E5%88%86%E9%A1%B5%E3%80%81%E5%93%8D%E5%BA%94%E8%BF%94%E5%9B%9E%E6%95%B0%E6%8D%AE%E5%92%8C%E8%AF%B7%E6%B1%82)%3C/span%3E" STYLE="fork"/>
+      <node TEXT="租借方案表单信息(显示在出租/借出页面)" ID="oxv2IxtX08" _mubu_text="%3Cspan%3E%E7%A7%9F%E5%80%9F%E6%96%B9%E6%A1%88%E8%A1%A8%E5%8D%95%E4%BF%A1%E6%81%AF(%E6%98%BE%E7%A4%BA%E5%9C%A8%E5%87%BA%E7%A7%9F/%E5%80%9F%E5%87%BA%E9%A1%B5%E9%9D%A2)%3C/span%3E" STYLE="fork"/>
+      <node TEXT="还有未知数据等变数可能需要解决" ID="VJQqf42vAk" _mubu_text="%3Cspan%20class=%22bold%20highlight-yellow%22%3E%E8%BF%98%E6%9C%89%E6%9C%AA%E7%9F%A5%E6%95%B0%E6%8D%AE%E7%AD%89%E5%8F%98%E6%95%B0%E5%8F%AF%E8%83%BD%E9%9C%80%E8%A6%81%E8%A7%A3%E5%86%B3%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="登陆界面表单信息" ID="1Sz6Wrxfxq" _mubu_text="%3Cspan%20class=%22bold%20text-color-yellow%22%3E%E7%99%BB%E9%99%86%E7%95%8C%E9%9D%A2%E8%A1%A8%E5%8D%95%E4%BF%A1%E6%81%AF%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="账户名" ID="ph8hinshTC" _mubu_text="%3Cspan%3E%E8%B4%A6%E6%88%B7%E5%90%8D%3C/span%3E" STYLE="fork"/>
+      <node TEXT="密码" ID="IjJCmnbGCU" _mubu_text="%3Cspan%3E%E5%AF%86%E7%A0%81%3C/span%3E" STYLE="fork"/>
+      <node TEXT="手机号" ID="KsEFaHkZbh" _mubu_text="%3Cspan%3E%E6%89%8B%E6%9C%BA%E5%8F%B7%3C/span%3E" STYLE="fork"/>
+      <node TEXT="验证码" ID="PVWfyIaZBh" _mubu_text="%3Cspan%3E%E9%AA%8C%E8%AF%81%E7%A0%81%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="注册界面信息" ID="DqdgQdifUZ" _mubu_text="%3Cspan%20class=%22bold%20text-color-yellow%22%3E%E6%B3%A8%E5%86%8C%E7%95%8C%E9%9D%A2%E4%BF%A1%E6%81%AF%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="账户名" ID="orTM5jbIjn" _mubu_text="%3Cspan%3E%E8%B4%A6%E6%88%B7%E5%90%8D%3C/span%3E" STYLE="fork"/>
+      <node TEXT="密码" ID="IGkZXISmXD" _mubu_text="%3Cspan%3E%E5%AF%86%E7%A0%81%3C/span%3E" STYLE="fork"/>
+      <node TEXT="二次密码" ID="EXXmyePbQO" _mubu_text="%3Cspan%3E%E4%BA%8C%E6%AC%A1%E5%AF%86%E7%A0%81%3C/span%3E" STYLE="fork"/>
+      <node TEXT="验证码" ID="4Xq5r5oVpE" _mubu_text="%3Cspan%3E%E9%AA%8C%E8%AF%81%E7%A0%81%3C/span%3E" STYLE="fork"/>
+      <node TEXT="实名注册" ID="gdf4toJLiB" _mubu_text="%3Cspan%3E%E5%AE%9E%E5%90%8D%E6%B3%A8%E5%86%8C%3C/span%3E" STYLE="fork"/>
+      <node TEXT="电动车绑定(可选，毕竟有人纯借电动车)" ID="T4QUHv0kIX" _mubu_text="%3Cspan%3E%E7%94%B5%E5%8A%A8%E8%BD%A6%E7%BB%91%E5%AE%9A(%E5%8F%AF%E9%80%89%EF%BC%8C%E6%AF%95%E7%AB%9F%E6%9C%89%E4%BA%BA%E7%BA%AF%E5%80%9F%E7%94%B5%E5%8A%A8%E8%BD%A6)%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="主界面" ID="JGH7tULMxI" _mubu_text="%3Cspan%20class=%22bold%20text-color-yellow%22%3E%E4%B8%BB%E7%95%8C%E9%9D%A2%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="图标信息(几号电动车、几号租借车、几号出租车)" ID="jRU7DGRC3i" _mubu_text="%3Cspan%3E%E5%9B%BE%E6%A0%87%E4%BF%A1%E6%81%AF(%E5%87%A0%E5%8F%B7%E7%94%B5%E5%8A%A8%E8%BD%A6%E3%80%81%E5%87%A0%E5%8F%B7%E7%A7%9F%E5%80%9F%E8%BD%A6%E3%80%81%E5%87%A0%E5%8F%B7%E5%87%BA%E7%A7%9F%E8%BD%A6)%3C/span%3E" STYLE="fork"/>
+      <node TEXT="警报信息(被破坏/信号发送器被拆/出校园/超时借入/超时借出)" ID="1wgo7IfRWh" _mubu_text="%3Cspan%3E%E8%AD%A6%E6%8A%A5%E4%BF%A1%E6%81%AF(%E8%A2%AB%E7%A0%B4%E5%9D%8F/%E4%BF%A1%E5%8F%B7%E5%8F%91%E9%80%81%E5%99%A8%E8%A2%AB%E6%8B%86/%E5%87%BA%E6%A0%A1%E5%9B%AD/%E8%B6%85%E6%97%B6%E5%80%9F%E5%85%A5/%E8%B6%85%E6%97%B6%E5%80%9F%E5%87%BA)%3C/span%3E" STYLE="fork"/>
+      <node TEXT="示例图标信息(红色主权/橙色借入/蓝色借出)(对应警报图标对应颜色)" ID="JcP4OF6JhQ" _mubu_text="%3Cspan%3E%E7%A4%BA%E4%BE%8B%E5%9B%BE%E6%A0%87%E4%BF%A1%E6%81%AF(%E7%BA%A2%E8%89%B2%E4%B8%BB%E6%9D%83/%E6%A9%99%E8%89%B2%E5%80%9F%E5%85%A5/%E8%93%9D%E8%89%B2%E5%80%9F%E5%87%BA)(%E5%AF%B9%E5%BA%94%E8%AD%A6%E6%8A%A5%E5%9B%BE%E6%A0%87%E5%AF%B9%E5%BA%94%E9%A2%9C%E8%89%B2)%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="出租/借出" ID="xc5EUL8Asc" _mubu_text="%3Cspan%20class=%22bold%20text-color-yellow%22%3E%E5%87%BA%E7%A7%9F/%E5%80%9F%E5%87%BA%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="选择租借方案界面信息" ID="Zg2rjjsQ14" _mubu_text="%3Cspan%20class=%22bold%20italic%20underline%22%3E%E9%80%89%E6%8B%A9%E7%A7%9F%E5%80%9F%E6%96%B9%E6%A1%88%E7%95%8C%E9%9D%A2%E4%BF%A1%E6%81%AF%3C/span%3E" STYLE="fork">
+        <node TEXT="显示账户A所发出的电动车商讨方案" ID="XuHxpFkkKV" _mubu_text="%3Cspan%20class=%22bold%20italic%20underline%22%3E%E6%98%BE%E7%A4%BA%E8%B4%A6%E6%88%B7A%E6%89%80%E5%8F%91%E5%87%BA%E7%9A%84%E7%94%B5%E5%8A%A8%E8%BD%A6%E5%95%86%E8%AE%A8%E6%96%B9%E6%A1%88%3C/span%3E" STYLE="fork">
+          <node TEXT="可以点击该账户显示该账户信息和信誉积分" ID="PI5yuXTyLM" _mubu_text="%3Cspan%3E%E5%8F%AF%E4%BB%A5%E7%82%B9%E5%87%BB%E8%AF%A5%E8%B4%A6%E6%88%B7%E6%98%BE%E7%A4%BA%E8%AF%A5%E8%B4%A6%E6%88%B7%E4%BF%A1%E6%81%AF%E5%92%8C%E4%BF%A1%E8%AA%89%E7%A7%AF%E5%88%86%3C/span%3E" STYLE="fork"/>
+          <node TEXT="可选择进入聊天界面或进行举报" ID="GiZHGgWwap" _mubu_text="%3Cspan%20class=%22bold%20italic%20underline%22%3E%E5%8F%AF%E9%80%89%E6%8B%A9%E8%BF%9B%E5%85%A5%E8%81%8A%E5%A4%A9%E7%95%8C%E9%9D%A2%E6%88%96%E8%BF%9B%E8%A1%8C%E4%B8%BE%E6%8A%A5%3C/span%3E" STYLE="fork">
+            <node TEXT="举报原因表单(举报成功可降低该账户信誉分)" ID="BZIfOIuCSj" _mubu_text="%3Cspan%3E%E4%B8%BE%E6%8A%A5%E5%8E%9F%E5%9B%A0%E8%A1%A8%E5%8D%95(%E4%B8%BE%E6%8A%A5%E6%88%90%E5%8A%9F%E5%8F%AF%E9%99%8D%E4%BD%8E%E8%AF%A5%E8%B4%A6%E6%88%B7%E4%BF%A1%E8%AA%89%E5%88%86)%3C/span%3E" STYLE="fork"/>
+          </node>
+        </node>
+        <node TEXT="电动车图片(车主自拍)" ID="cKWOzugb6b" _mubu_text="%3Cspan%3E%E7%94%B5%E5%8A%A8%E8%BD%A6%E5%9B%BE%E7%89%87(%E8%BD%A6%E4%B8%BB%E8%87%AA%E6%8B%8D)%3C/span%3E" STYLE="fork"/>
+        <node TEXT="可自定义电动车租借方案表单" ID="TIWAmrhves" _mubu_text="%3Cspan%20class=%22bold%20italic%20underline%22%3E%E5%8F%AF%E8%87%AA%E5%AE%9A%E4%B9%89%E7%94%B5%E5%8A%A8%E8%BD%A6%E7%A7%9F%E5%80%9F%E6%96%B9%E6%A1%88%E8%A1%A8%E5%8D%95%3C/span%3E" STYLE="fork">
+          <node TEXT="显示正在商讨方案的价格/月(可更改)" ID="QJNLRWZ15a" _mubu_text="%3Cspan%3E%E6%98%BE%E7%A4%BA%E6%AD%A3%E5%9C%A8%E5%95%86%E8%AE%A8%E6%96%B9%E6%A1%88%E7%9A%84%E4%BB%B7%E6%A0%BC/%E6%9C%88(%E5%8F%AF%E6%9B%B4%E6%94%B9)%3C/span%3E" STYLE="fork"/>
+          <node TEXT="添加车图片、信息 " ID="reaYDseN4R" _mubu_text="%3Cspan%3E%E6%B7%BB%E5%8A%A0%E8%BD%A6%E5%9B%BE%E7%89%87%E3%80%81%E4%BF%A1%E6%81%AF%20%3C/span%3E" STYLE="fork"/>
+        </node>
+        <node TEXT="可输入唯一账户uid进行借出" ID="KPLf8kaRyZ" _mubu_text="%3Cspan%3E%E5%8F%AF%E8%BE%93%E5%85%A5%E5%94%AF%E4%B8%80%E8%B4%A6%E6%88%B7uid%E8%BF%9B%E8%A1%8C%E5%80%9F%E5%87%BA%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="账户名下管理电动车表单" ID="0AQPSUXDcF" _mubu_text="%3Cspan%20class=%22bold%20italic%20underline%22%3E%E8%B4%A6%E6%88%B7%E5%90%8D%E4%B8%8B%E7%AE%A1%E7%90%86%E7%94%B5%E5%8A%A8%E8%BD%A6%E8%A1%A8%E5%8D%95%3C/span%3E" STYLE="fork">
+        <node TEXT="显示车牌" ID="e3DWZmI8SY" _mubu_text="%3Cspan%3E%E6%98%BE%E7%A4%BA%E8%BD%A6%E7%89%8C%3C/span%3E" STYLE="fork"/>
+        <node TEXT="出租/借出时间" ID="GHibK5MyNz" _mubu_text="%3Cspan%3E%E5%87%BA%E7%A7%9F/%E5%80%9F%E5%87%BA%E6%97%B6%E9%97%B4%3C/span%3E" STYLE="fork"/>
+        <node TEXT="状态(正常/异常)" ID="ZPceDcaiNZ" _mubu_text="%3Cspan%3E%E7%8A%B6%E6%80%81(%E6%AD%A3%E5%B8%B8/%E5%BC%82%E5%B8%B8)%3C/span%3E" STYLE="fork"/>
+        <node TEXT="处理警报信息(忽略/举报借车、租车账户)" ID="NUyh14B7qF" _mubu_text="%3Cspan%3E%E5%A4%84%E7%90%86%E8%AD%A6%E6%8A%A5%E4%BF%A1%E6%81%AF(%E5%BF%BD%E7%95%A5/%E4%B8%BE%E6%8A%A5%E5%80%9F%E8%BD%A6%E3%80%81%E7%A7%9F%E8%BD%A6%E8%B4%A6%E6%88%B7)%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="用户锁车(不知道如何实现)" ID="U0gvOXlq0y" _mubu_text="%3Cspan%20class=%22bold%20italic%20underline%20highlight-yellow%22%3E%E7%94%A8%E6%88%B7%E9%94%81%E8%BD%A6(%E4%B8%8D%E7%9F%A5%E9%81%93%E5%A6%82%E4%BD%95%E5%AE%9E%E7%8E%B0)%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="个人信息" ID="SfGcsT4OQt" _mubu_text="%3Cspan%20class=%22bold%20text-color-yellow%22%3E%E4%B8%AA%E4%BA%BA%E4%BF%A1%E6%81%AF%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="头像" ID="rGSzGU6YcD" _mubu_text="%3Cspan%3E%E5%A4%B4%E5%83%8F%3C/span%3E" STYLE="fork"/>
+      <node TEXT="uid" ID="KP7STpQnlq" _mubu_text="%3Cspan%3Euid%3C/span%3E" STYLE="fork"/>
+      <node TEXT="名字" ID="c3BDQZuWbv" _mubu_text="%3Cspan%3E%E5%90%8D%E5%AD%97%3C/span%3E" STYLE="fork"/>
+      <node TEXT="绑定邮箱" ID="Y3VvNw8LPo" _mubu_text="%3Cspan%3E%E7%BB%91%E5%AE%9A%E9%82%AE%E7%AE%B1%3C/span%3E" STYLE="fork"/>
+      <node TEXT="绑定手机号" ID="kkFjibq6u2" _mubu_text="%3Cspan%3E%E7%BB%91%E5%AE%9A%E6%89%8B%E6%9C%BA%E5%8F%B7%3C/span%3E" STYLE="fork"/>
+      <node TEXT="绑定IP地址" ID="GZzVB254Uk" _mubu_text="%3Cspan%3E%E7%BB%91%E5%AE%9AIP%E5%9C%B0%E5%9D%80%3C/span%3E" STYLE="fork"/>
+      <node TEXT="身份证" ID="c7kW96NjgL" _mubu_text="%3Cspan%3E%E8%BA%AB%E4%BB%BD%E8%AF%81%3C/span%3E" STYLE="fork"/>
+      <node TEXT="绑定车" ID="MeHVAStqky" _mubu_text="%3Cspan%3E%E7%BB%91%E5%AE%9A%E8%BD%A6%3C/span%3E" STYLE="fork"/>
+      <node TEXT="关于(相关法律法规信息)" ID="foB54uSHku" _mubu_text="%3Cspan%3E%E5%85%B3%E4%BA%8E(%E7%9B%B8%E5%85%B3%E6%B3%95%E5%BE%8B%E6%B3%95%E8%A7%84%E4%BF%A1%E6%81%AF)%3C/span%3E" STYLE="fork"/>
+    </node>
+    <node TEXT="时间安排" ID="1qkurfbT4K" _mubu_text="%3Cspan%3E%E6%97%B6%E9%97%B4%E5%AE%89%E6%8E%92%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="(理想状态):" ID="0XThxxXhbD" _mubu_text="%3Cspan%3E(%E7%90%86%E6%83%B3%E7%8A%B6%E6%80%81):%3C/span%3E" STYLE="fork">
+        <node TEXT="搜集资料，构建实现技术如何实现框架(1月)" ID="WdYqVUBwjw" _mubu_text="%3Cspan%3E%E6%90%9C%E9%9B%86%E8%B5%84%E6%96%99%EF%BC%8C%E6%9E%84%E5%BB%BA%E5%AE%9E%E7%8E%B0%E6%8A%80%E6%9C%AF%E5%A6%82%E4%BD%95%E5%AE%9E%E7%8E%B0%E6%A1%86%E6%9E%B6(1%E6%9C%88)%3C/span%3E" STYLE="fork"/>
+        <node TEXT="设计每天交稿，同时后端开始写接口文档-&gt;前端开始编辑页面(春节前)" ID="7XP0EQ5g2h" _mubu_text="%3Cspan%3E%E8%AE%BE%E8%AE%A1%E6%AF%8F%E5%A4%A9%E4%BA%A4%E7%A8%BF%EF%BC%8C%E5%90%8C%E6%97%B6%E5%90%8E%E7%AB%AF%E5%BC%80%E5%A7%8B%E5%86%99%E6%8E%A5%E5%8F%A3%E6%96%87%E6%A1%A3-&amp;gt;%E5%89%8D%E7%AB%AF%E5%BC%80%E5%A7%8B%E7%BC%96%E8%BE%91%E9%A1%B5%E9%9D%A2(%E6%98%A5%E8%8A%82%E5%89%8D)%3C/span%3E" STYLE="fork"/>
+        <node TEXT="前端编辑完页面后端联调数据，完善功能(春节后)" ID="AatOzlzTq5" _mubu_text="%3Cspan%3E%E5%89%8D%E7%AB%AF%E7%BC%96%E8%BE%91%E5%AE%8C%E9%A1%B5%E9%9D%A2%E5%90%8E%E7%AB%AF%E8%81%94%E8%B0%83%E6%95%B0%E6%8D%AE%EF%BC%8C%E5%AE%8C%E5%96%84%E5%8A%9F%E8%83%BD(%E6%98%A5%E8%8A%82%E5%90%8E)%3C/span%3E" STYLE="fork"/>
+        <node TEXT="开始写答辩材料(3月初)" ID="X68akEJI3a" _mubu_text="%3Cspan%3E%E5%BC%80%E5%A7%8B%E5%86%99%E7%AD%94%E8%BE%A9%E6%9D%90%E6%96%99(3%E6%9C%88%E5%88%9D)%3C/span%3E" STYLE="fork"/>
+        <node TEXT="写完答辩材料三月中旬交作品" ID="3V106MdPYI" _mubu_text="%3Cspan%3E%E5%86%99%E5%AE%8C%E7%AD%94%E8%BE%A9%E6%9D%90%E6%96%99%E4%B8%89%E6%9C%88%E4%B8%AD%E6%97%AC%E4%BA%A4%E4%BD%9C%E5%93%81%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="事后工作：" ID="H1I9OvNdH7" _mubu_text="%3Cspan%20class=%22italic%20text-color-blue%22%3E%E4%BA%8B%E5%90%8E%E5%B7%A5%E4%BD%9C%EF%BC%9A%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="准备答辩材料" ID="RLkfUjwuBk" _mubu_text="%3Cspan%3E%E5%87%86%E5%A4%87%E7%AD%94%E8%BE%A9%E6%9D%90%E6%96%99%3C/span%3E" STYLE="fork">
+        <node TEXT="程序示例PPT" ID="9v7MAaRF79" _mubu_text="%3Cspan%3E%E7%A8%8B%E5%BA%8F%E7%A4%BA%E4%BE%8BPPT%3C/span%3E" STYLE="fork"/>
+        <node TEXT="程序示例视频" ID="c27dM9ItKE" _mubu_text="%3Cspan%3E%E7%A8%8B%E5%BA%8F%E7%A4%BA%E4%BE%8B%E8%A7%86%E9%A2%91%3C/span%3E" STYLE="fork"/>
+        <node TEXT="程序示例报告" ID="Pps460PRVc" _mubu_text="%3Cspan%3E%E7%A8%8B%E5%BA%8F%E7%A4%BA%E4%BE%8B%E6%8A%A5%E5%91%8A%3C/span%3E" STYLE="fork"/>
+        <node TEXT="预期未来展现的功能云云..." ID="zCfvENypET" _mubu_text="%3Cspan%20class=%22strikethrough%22%3E%E9%A2%84%E6%9C%9F%E6%9C%AA%E6%9D%A5%E5%B1%95%E7%8E%B0%E7%9A%84%E5%8A%9F%E8%83%BD%E4%BA%91%E4%BA%91...%3C/span%3E" STYLE="fork"/>
+      </node>
+    </node>
+  </node>
+</map>
